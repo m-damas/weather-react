594130f6f3e3a28404920aa8f8be80879284fe8f
m-damas <145788515+m-damas@users.noreply.github.com>
2023-12-01 15:29:53 -0600

    updated icon url

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -16,7 +16,7 @@ export default function WeatherApp(props) {
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
   function search() {
