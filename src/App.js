af363eb49f84ea3b5a29f36217a72a3ab43d56cd
m-damas <145788515+m-damas@users.noreply.github.com>
2023-12-01 09:16:48 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import WeatherApp from "./WeatherApp";
 function App() {
   return (
     <div className="App">
-      <WeatherApp />
-      <p>
-        <a
-          href="https://github.com/m-damas/weather-design"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          <b>Open-source code</b>{" "}
-        </a>
-        by Melissa Damas
-      </p>
+      <div className="container">
+        <WeatherApp />
+        <p>
+          <a
+            href="https://github.com/m-damas/weather-design"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <b>Open-source code</b>{" "}
+          </a>
+          by Melissa Damas
+        </p>
+      </div>
     </div>
   );
 }
